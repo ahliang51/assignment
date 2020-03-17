e98e4f9d25f087b5a2793730f760cde045a2beb4
shengliang <ahliang51@hotmail.com>
2020-03-17 13:44:57 +0800

    updated Q1B

--- Q1B.java
+++ Q1B.java
@@ -1,26 +1,31 @@
-   public A{
-    int value;
+import java.util.ArrayList;
+import java.util.List;
+
+class A {
+   int value;
    List<Integer> list;
 
-   public A(int value){
-   this.value = value;
-   list.add(this.value);
+   public A(int value) {
+      this.value = value;
+      this.list = new ArrayList<>();
+      list.add(this.value);
    }
 
-   public A(List<Integer> list){
-    this.list = list;
+   public A(List<Integer> list) {
+      this.list = list;
    }
 
-   public A next(int value){
-   List<Integer> newList = this.list;
-   return new A(newList.add(value));
+   public A next(int value) {
+      List<Integer> newList = new ArrayList<>(this.list);
+      newList.add(value);
+      return new A(newList);
    }
 
-   public String toString(){
-   String temp = "";
-   for(Integer value: this.list){
-   temp += "[A:" + value +"]";
+   public String toString() {
+      String temp = "";
+      for (Integer value : this.list) {
+         temp += "[A:" + value + "]";
+      }
+      return temp;
    }
-   return temp;
-   }
-   }
+}

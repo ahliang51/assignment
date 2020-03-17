aa7eb4dee7937a2cd972703233f35664b3024b26
shengliang <ahliang51@hotmail.com>
2020-03-17 16:12:24 +0800

    updated 1d

--- Q1D.java
+++ Q1D.java
@@ -1,18 +1,24 @@
-   class D<T extends E> {
+import java.util.ArrayList;
+import java.util.List;
 
-   private ArrayList<T> list = new ArrayList<>();
+class D {
 
-   public D(){
+   public D() {
    }
 
-   public static <T>  D<T> add(ArrayList<T> list, T t){
-   D d = new D<>();
-   d.list.addAll(list);
-   d.list.add(t);
-   return d;
+   public static <T> List<T> add(List<T> list, T t) {
+      list.add(t);
+      return list;
    }
 
-   public static List join(List<T> list1,List<T>list2){
-    d.list.addAll(list);
+   public static <T, E extends T> List<T> join(List<T> list1, List<E> list2) {
+      if (list1 == list2) {
+         return list1;
+      } else {
+         List<T> temp = new ArrayList<>();
+         temp.addAll(list1);
+         temp.addAll(list2);
+         return temp;
+      }
    }
-   }
+}

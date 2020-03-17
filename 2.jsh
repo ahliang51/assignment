9898e2943e05fc1f59d1f9bb07a78920f41e8f7b
shengliang <ahliang51@hotmail.com>
2020-03-17 18:33:41 +0800

    updated Q2

---
+++ 2.jsh
@@ -0,0 +1,6 @@
+/open Q2.java
+String s1 = new ToString<>().cast(123);
+String s2 = new ToString<>().cast(List.of(1,2,3))
+String s3 = new ToString<>().cast(Map.of("COM1",117417))
+/var
+/exit
--- Q2.java
+++ Q2.java
@@ -1,35 +1,45 @@
-   import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-   interface TypeCaster<S,T>{
-   public T cast(S s,T t){}
+interface TypeCaster<S, T> {
+   public T cast(S s);
+}
+
+class ToString<S> implements TypeCaster<S, String> {
+
+   public String cast(S s) {
+      return s.toString();
    }
 
-   class ToString<S> implements TypeCaster<String,String>{
+}
 
-   public String cast(String s,String t){
-   return s;
+class Round implements TypeCaster<Double, Integer> {
+
+   public Integer cast(Double s) {
+      return (int) Math.round(s);
    }
 
+}
+
+class ToList<T> implements TypeCaster<T[], List<T>> {
+
+   public List<T> cast(T[] s) {
+      return Arrays.asList(s);
+
+   }
+}
+
+class ListCaster {
+
+   static <S, T> List<T> castList(List<S> s, TypeCaster<S, T> t) {
+      List<T> newList = new ArrayList<>();
+      for (S temp : s) {
+         newList.add(t.cast(temp));
+      }
+
+      return newList;
 
    }
 
-   class Round implements TypeCaster<Double,Integer>{
-
-   public Integer cast(Double s,Integer t){
-   return (int)Math.round(s);
-   }
-   }
-
-   public ToList<T> implements TypeCaster<T[],List<T>>{
-
-   public List<T> cast(T[] s,List<T> t){
-   return Arrays.asList(s);
-   }
-
-   }
-
-   public ListCaster{
-    static <S,T extends TypeCaster> castList(S s, T t){
-
-   }
-   }
+}
